7141475c2bade43d657ddd79f8031b02fa76d1fd
gary.y <s14031403@gmail.com>
2023-03-21 20:45:11 -0400

    compile different binary crate  using cargo

---
+++ src/bin/client.rs
@@ -0,0 +1,5 @@
+// you can build this file by running `cargo build --bin client`
+// because cargo will look for a file named `client.rs` in the `src/bin` directory
+fn main() {
+    println!("Hello, world!");
+}
---
+++ src/daemon/bin/main.rs
@@ -0,0 +1,8 @@
+// you can build this file by running `cargo build --bin daemon`
+// because this file is described in Cargo.toml:
+// [bin]
+// name = "daemon"
+// path = "src/daemon/bin/main.rs"
+fn main() {
+    println!("Hello, world!");
+}
