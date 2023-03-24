7f476e423a2b2f21b6e0846c064e7ace8e543394
gary.y <s14031403@gmail.com>
2023-03-24 12:08:20 -0400

    test

--- package_and_crate/src/main.rs
+++ package_and_crate/src/main.rs
@@ -1,6 +1,6 @@
 fn main() {
     // 这里 Vec<T> 在调用 iter() 时被解引用成 &[T]，所以可以访问 iter()
-    let result = vec![1, 2, 3, 4]
+    let result = vec![1, 2, 3, 4, 5]
         // return a iterator
         .iter()
         // return a type which impl Iterator
