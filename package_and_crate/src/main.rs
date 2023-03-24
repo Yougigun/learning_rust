169aa4e145f63e9b9e314c0aa4c3ca7f332bcc42
gary.y <s14031403@gmail.com>
2023-03-24 15:34:46 -0400

    folder structure of multi binary crate

--- package_and_crate/src/main.rs
+++ package_and_crate/src/main.rs
@@ -1,3 +1,8 @@
+// multiple bin crate
+// cargo build --bin package_and_crate
+// cargo build --bin daemon
+// cargo build --bin client
+
 fn main() {
     // 这里 Vec<T> 在调用 iter() 时被解引用成 &[T]，所以可以访问 iter()
     let result = vec![1, 2, 3, 4, 5]
